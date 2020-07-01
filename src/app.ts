29400c6d6f10f2b898261176ed4fafe1be8652bc
Fernando López <ferlopezm94@gmail.com>
2020-07-01 00:00:47 -0500

    feat(app): get forecast data of given location

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,21 @@
-export const greetingUppercase = (name: string): string => {
-  const uppercase = `Hello ${name}!`.toUpperCase();
-  console.log(uppercase);
-  return uppercase;
+import { geocode } from './utils/geocode';
+import { forecast } from './utils/forecast';
+
+const main = async () => {
+  const address = process.argv[2];
+
+  if (!address) {
+    return console.log('Please provide a location.');
+  }
+
+  try {
+    const { latitude, longitude, location } = await geocode(address);
+    const forecastData = await forecast(latitude, longitude);
+
+    console.log(`${location}.\n${forecastData}`);
+  } catch (error) {
+    console.error('error :>> ', error);
+  }
 };
+
+main();
