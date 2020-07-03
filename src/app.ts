dcfb48843962c7f09b34befb4a50d8dabdfdfb99
Fernando López <ferlopezm94@gmail.com>
2020-07-02 19:32:24 -0500

    feat(app): setup express server and routes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,24 @@
+import express from 'express';
+import hbs from 'hbs';
+import path from 'path';
+
 import { geocode } from './utils/geocode';
 import { forecast } from './utils/forecast';
+
+const app = express();
+
+// Define paths for express configuration
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsDirectoryPath = path.join(__dirname, '../templates/views');
+const partialsDirectoryPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsDirectoryPath);
+hbs.registerPartials(partialsDirectoryPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
 
 const main = async () => {
   const address = process.argv[2];
@@ -19,3 +38,50 @@ const main = async () => {
 };
 
 main();
+
+app.get('', (_req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'John Doe',
+  });
+});
+
+app.get('/about', (_req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'John Doe',
+  });
+});
+
+app.get('/help', (_req, res) => {
+  res.render('help', {
+    title: 'Help',
+    name: 'John Doe',
+    message: 'We are here for you!',
+  });
+});
+
+app.get('/weather', (_req, res) => {
+  res.send({
+    forecast: 'It is raining',
+    location: 'Chiapas',
+  });
+});
+
+app.get('/help/*', (_req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'John Doe',
+    errorMessage: 'Help article not found.',
+  });
+});
+
+app.get('*', (_req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'John Doe',
+    errorMessage: 'Page not found.',
+  });
+});
+
+app.listen(3000, () => console.log('Server up and running!'));
