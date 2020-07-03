ca24cff00522a60b0b5a820ec22aad1d59454535
Fernando López <ferlopezm94@gmail.com>
2020-07-02 23:40:16 -0500

    feat(index): add search and display weather data

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,25 @@
 console.log('Client side JavaScript file is loaded.');
+
+const weatherForm = document.querySelector('form');
+const searchInput = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+weatherForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const address = searchInput.value;
+
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
+
+  fetch(`http://localhost:3000/weather?address=${address}`).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
+});
